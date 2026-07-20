770edca41ba34f46a347765576101c9aae140a5d
Ayush Kulkarni <71148799+ayush-kulkarni@users.noreply.github.com>
2026-07-20 09:52:30 -0500

    optimized for SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,16 @@ import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   srcDir: "./src",
 
   vite: {
     plugins: [tailwindcss()],
-  }
+  },
+
+  site: "https://ayushkulkarni.com",
+  integrations: [sitemap()],
 });
